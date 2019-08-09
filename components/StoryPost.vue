5e625ef7cc243a8ba73b4eddaba17355f5636436
Radames Ajna <radamajna@gmail.com>
2019-08-09 14:59:43 -0700

    first commit

---
+++ components/StoryPost.vue
@@ -0,0 +1,81 @@
+<template>
+  <div
+    class="post-container"
+    :style="{backgroundColor: postData.secondary_color}"
+    @mouseover="toggle('play')"
+    @mouseleave="toggle('pause')"
+  >
+    <div class="box">
+      <img :src="postData.featured_image" class="img-fluid">
+      <div class="title">
+        <h3 v-html="postData.title" />
+      </div>
+    </div>
+    <audio
+      ref="player"
+      controlshidden=""
+      preload="none"
+      :src="postData.feature_audio"
+      type="audio/mp3"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.box{
+  position: relative;
+}
+.title {
+  position: absolute;
+  bottom: 0px;
+  color: white;
+  /* background-color: white; */
+  text-shadow: 0px 2px #00000094;
+}
+h3{
+  font-size:  1em;
+  margin: 0.5em;
+}
+.post-container {
+  position: relative;
+  width: calc(100vw/5);
+}
+.img-fluid{
+  width: 100%;
+  height: auto;
+}
+.img-box{
+  max-height: calc(100vh/6);
+}
+</style>
+
+<script>
+export default {
+  name: 'StoryPost',
+
+  props: {
+    postData: {
+      type: Object,
+      required: true,
+      default: null
+    }
+  },
+  data () {
+    return {
+      test: 'TEST'
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    toggle (mode) {
+      if (mode === 'play') {
+        this.$refs.player.play()
+      } else {
+        this.$refs.player.pause()
+      }
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="grid">
+    <div
+      v-for="post in postsData"
+      :key="post.id"
+      class="item"
+    >
+      <a :href="post.link" target="_blanket">
+        <StoryPost :post-data="post" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import StoryPost from '~/components/StoryPost.vue'
+// import DATA_LINKS from '~/data/data.json'
+
+export default {
+  components: {
+    StoryPost
+  },
+  data () {
+    return {
+      postsDataRaw: [],
+      grid: null
+    }
+  },
+  computed: {
+    postsData () {
+      return this.postsDataRaw.map(e => ({
+        featured_image: e.episode_featured_image,
+        feature_audio: e.acf.featured_audio,
+        secondary_color: e.acf.secondary_color,
+        primary_color: e.acf.primary_color,
+        content: e.content,
+        date: e.date,
+        link: e.link,
+        id: e.id,
+        title: e.title.rendered
+      }))
+    }
+
+  },
+  mounted () {
+    this.fetchData(31).then(() => {
+      this.startMuuri()
+    })
+  },
+  methods: {
+    async fetchData (perPage) {
+      this.postsDataRaw = await fetch(`https://yr.media/wp-json/wp/v2/posts?outlets=94&per_page=${perPage}`)
+        .then(d => d.json())
+    },
+    startMuuri () {
+    /* globals Muuri */
+      this.grid = new Muuri('.grid', {
+        layout: {
+          fillGaps: true,
+          horizontal: false,
+          alignRight: false,
+          alignBottom: false,
+          rounding: false
+        },
+        dragEnabled: false,
+        showDuration: 0,
+        hideDuration: 0,
+        layoutDuration: 0
+
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.grid {
+  position: relative;
+}
+.item {
+  display: block;
+  position: absolute;
+  // width: 100px;
+  // height: 100px;
+  // margin: 5px;
+  // z-index: 1;
+  // background: #000;
+  // color: #fff;
+}
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
