eb53b0078e0d24be8d0ca827c84e983203e6d207
Radames Ajna <radamajna@gmail.com>
2019-08-10 00:54:29 -0700

    audio player progress

--- components/StoryPost.vue
+++ components/StoryPost.vue
@@ -6,7 +6,7 @@
     @mouseleave="toggle('pause')"
   >
     <div class="box">
-      <img :src="postData.featured_image" class="img-fluid">
+      <img :src="postData.featured_image" class="img-fluid z-image">
       <div class="title">
         <h3 v-html="postData.title" />
       </div>
@@ -18,6 +18,8 @@
       :src="postData.feature_audio"
       type="audio/mp3"
     />
+    <div ref="progressLayer" class="post-container-layer" />
+    <div ref="progressLayerRight" class="post-container-layer layer-rigth" />
   </div>
 </template>
 
@@ -31,6 +33,10 @@
   color: white;
   /* background-color: white; */
   text-shadow: 0px 2px #00000094;
+  z-index: 100;
+}
+.z-image{
+  z-index: 0;
 }
 h3{
   font-size:  1em;
@@ -39,6 +45,25 @@ h3{
 .post-container {
   position: relative;
   width: calc(100vw/5);
+}
+ .post-container-layer {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0px;
+    height: 100%;
+    background-image: linear-gradient(120deg, #eaee44bf, #33d0ff82);
+    opacity: 1;
+    z-index: 10;
+    @supports (mix-blend-mode: hard-light) {
+        mix-blend-mode: hard-light;
+    }
+  }
+.layer-rigth{
+  right: 0;
+  left: unset;
+  background-image: linear-gradient(60deg, #33d0ff82, #eaee44bf) !important;
 }
 .img-fluid{
   width: 100%;
@@ -70,11 +95,19 @@ export default {
   },
   methods: {
     toggle (mode) {
+      const player = this.$refs.player
       if (mode === 'play') {
-        this.$refs.player.play()
+        player.play()
+        player.addEventListener('timeupdate', this.onPlay, true)
       } else {
-        this.$refs.player.pause()
+        player.removeEventListener('timeupdate', this.onPlay, true)
+        player.pause()
       }
+    },
+    onPlay () {
+      const player = this.$refs.player
+      this.$refs.progressLayer.style.width = `${50 * player.currentTime / player.duration}%`
+      this.$refs.progressLayerRight.style.width = `${50 * player.currentTime / player.duration}%`
     }
   }
 }
